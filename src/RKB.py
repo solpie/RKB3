b2d7d15180a0f26ff59a9283144ef77d23462797
solpie <solpie.net@gmail.com>
2016-11-30 18:26:48 +0800

    inti

---
+++ src/RKB.py
@@ -0,0 +1,80 @@
+import os
+# print(os.path.realpath(__file__))
+# server_path = os.path.realpath(__file__)
+serverConf = {"port": 80, "path": '.',
+              "views": [], "reqHeaders": [], "resHeaders": []}
+
+serverConf["path"] = os.path.dirname(__file__)
+if serverConf["path"] == "":
+    serverConf["path"] = os.path.realpath(".")
+print("server root:", serverConf["path"])
+# config
+import configparser
+
+config = configparser.RawConfigParser()
+
+
+def loadConf():
+    config.read(os.path.join(serverConf["path"], '.cfg'))
+    serverConf["port"] = config.get('server', 'port')
+    serverConf["views"] = config.get('server', 'views')
+    serverConf["reqHeaders"] = config.get('server', 'reqHeaders').split(",")
+    serverConf["resHeaders"] = config.get('server', 'resHeaders').split(",")
+    print("serverConf:", serverConf)
+
+loadConf()
+    
+# web server
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+from engineio import async_eventlet
+async_mode ="eventlet"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+namspace_rkb
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+
+if __name__ == '__main__':
+    socketio.run(app,port=int(serverConf["port"]), debug=True)
