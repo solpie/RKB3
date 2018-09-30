f48b40408d813c84353086a123a4da6c0e159e6f
solpie <solpie.net@gmail.com>
2018-09-30 19:16:19 +0800

    show bottom

--- src/client/view/panel/stageOnline/scoreV2/BottomNotice.ts
+++ src/client/view/panel/stageOnline/scoreV2/BottomNotice.ts
@@ -36,35 +36,42 @@ export class BottomNotice extends BasePanel {
 
     }
     _show(data) {
-        let nameMap = {
-            '1': ['抖音:路人王篮球', '快手:路人王篮球'],
-            '2': ['今日头条:路人王篮球', '西瓜视频:路人王篮球'],
-            '3': ['微博:路人王篮球', '微信:虎扑路人王'],
-            '4': ['优酷:路人王篮球', 'UC:路人王篮球'],
-            '5': ['B站:路人王篮球', '微视:路人王篮球'],
-            '6': ['腾讯视频:路人王篮球', 'YouTube:路人王篮球'],
-            '7': ['触电新闻:路人王篮球', '网易新闻:路人王篮球'],
+        if (data.visible) {
+            let nameMap = {
+                '1': ['抖音:路人王篮球', '快手:路人王篮球'],
+                '2': ['今日头条:路人王篮球', '西瓜视频:路人王篮球'],
+                '3': ['微博:路人王篮球', '微信:虎扑路人王'],
+                '4': ['优酷:路人王篮球', 'UC:路人王篮球'],
+                '5': ['B站:路人王篮球', '微视:路人王篮球'],
+                '6': ['腾讯视频:路人王篮球', 'YouTube:路人王篮球'],
+                '7': ['触电新闻:路人王篮球', '网易新闻:路人王篮球'],
+            }
+            let t = nameMap[data.idx]
+            this.lName.setText(t[0])
+            this.rName.setText(t[1])
+            let lUrl = '/img/panel/notice/appLogo/' + data.idx + '_0.png'
+            let rUrl = '/img/panel/notice/appLogo/' + data.idx + '_1.png'
+            imgLoader.loadTexArr([lUrl, rUrl], _ => {
+                console.log('load app logo')
+                this.iconCtn.removeChildren()
+                let lIcon = newBitmap({ url: lUrl })
+                lIcon.x = 450
+                this.iconCtn.addChild(lIcon)
+                lIcon = newBitmap({ url: rUrl })
+                lIcon.x = 980
+                this.iconCtn.addChild(lIcon)
+                // this.rIcon.texture = imgLoader.getTexBySp(rUrl)
+
+                // this.lIcon.texture = imgLoader.getTexBySp(lUrl)
+                this.rIcon.texture = imgLoader.getTexBySp(rUrl)
+            })
+
+            this.p.addChild(this)
+
         }
-        let t = nameMap[data.idx]
-        this.lName.setText(t[0])
-        this.rName.setText(t[1])
-        let lUrl = '/img/panel/notice/appLogo/' + data.idx + '_0.png'
-        let rUrl = '/img/panel/notice/appLogo/' + data.idx + '_1.png'
-        imgLoader.loadTexArr([lUrl, rUrl], _ => {
-            console.log('load app logo')
-            this.iconCtn.removeChildren()
-            let lIcon = newBitmap({ url: lUrl })
-            lIcon.x = 450
-            this.iconCtn.addChild(lIcon)
-            lIcon = newBitmap({ url: rUrl })
-            lIcon.x = 980
-            this.iconCtn.addChild(lIcon)
-            // this.rIcon.texture = imgLoader.getTexBySp(rUrl)
+        else {
+            this.hide(data)
+        }
 
-            // this.lIcon.texture = imgLoader.getTexBySp(lUrl)
-            this.rIcon.texture = imgLoader.getTexBySp(rUrl)
-        })
-
-        this.p.addChild(this)
     }
 }
