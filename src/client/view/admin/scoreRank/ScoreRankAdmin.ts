40ecef7f0145871956b1ffadd9ef60c8ea3c87e1
solpie <solpie.net@gmail.com>
2018-10-18 12:05:16 +0800

    sort total score

--- src/client/view/admin/scoreRank/ScoreRankAdmin.ts
+++ src/client/view/admin/scoreRank/ScoreRankAdmin.ts
@@ -84,18 +84,24 @@ class _ScoreRankAdmin extends VueBase {
                     let p2 = rec.player[1]
                     if (!winMap[p1]) {
                         winMap[p1] = []
-                        totalScoreMap[p1] = 0
+                        totalScoreMap[p1] = { score: 0, w: 0, l: 0 }
                     }
                     if (!winMap[p2]) {
-                        totalScoreMap[p2] = 0
+                        totalScoreMap[p2] = { score: 0, w: 0, l: 0 }
                         winMap[p2] = []
                     }
-                    totalScoreMap[p1] += rec.score[0] - rec.score[1]
-                    totalScoreMap[p2] -= rec.score[0] - rec.score[1]
-                    if (rec.score[0] > rec.score[1])
+                    totalScoreMap[p1].score += rec.score[0] - rec.score[1]
+                    totalScoreMap[p2].score -= rec.score[0] - rec.score[1]
+                    if (rec.score[0] > rec.score[1]) {
+                        totalScoreMap[p1].w += 1
+                        totalScoreMap[p2].l += 1
                         winMap[p1].push(p2)
-                    else
+                    }
+                    else {
+                        totalScoreMap[p2].w += 1
+                        totalScoreMap[p1].l += 1
                         winMap[p2].push(p1)
+                    }
 
                     rec.name = [playerMap[p1].name, playerMap[p2].name]
                     rec.gameIdx = idx
@@ -117,7 +123,7 @@ class _ScoreRankAdmin extends VueBase {
                 _(data1)
             })
     }
-    createOption(data,callback?) {
+    createOption(data, callback?) {
         let a = [];
         let playerMap = data.playerMap
         this.playerMap = playerMap
@@ -257,7 +263,6 @@ class _ScoreRankAdmin extends VueBase {
         },
         onCreateGame() {
             console.log('onCreateGame', this.lPlayer, this.rPlayer)
-
             let a = this.vsPlayer.split(' ')
             let p1 = a[0]
             let p2 = a[1]
